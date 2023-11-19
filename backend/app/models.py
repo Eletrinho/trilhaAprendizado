b60e0704503a6635d1be92ec8a49857308b6f4dd
Cauã <101751692+Eletrinho@users.noreply.github.com>
2023-11-19 13:27:57 -0300

    Skills models created and migrated

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+class Skill(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    confidence_about = models.PositiveSmallIntegerField(validators=[
+                MaxValueValidator(5),
+                MinValueValidator(1)])
+
+    def _str_(self):
+        return self.name
+
+class SubSkill(models.Model):
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    confidence_about = models.PositiveSmallIntegerField(validators=[
+                MaxValueValidator(5),
+                MinValueValidator(1)])
+
+    def _str_(self):
+        return self.name
